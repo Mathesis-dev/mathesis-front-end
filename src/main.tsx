e3703a658b483456bc7e8ae60824df5da6f2b736
Geovane <geovanegallotticazal@hotmail.com>
2023-03-27 10:58:33 -0300

    🚧 landing page (not responsive)

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,19 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import Router from "./routes";
 
+import styled from "@emotion/styled";
+import "@fontsource/fira-sans";
+
+const AppContainer = styled.div`
+  font-family: "Fira Sans";
+`;
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
-      <RouterProvider router={Router} />
+      <AppContainer>
+        <RouterProvider router={Router} />
+      </AppContainer>
     </ChakraProvider>
   </React.StrictMode>
 );
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,7 +1,19 @@
-import { Button, Flex, Text } from "@chakra-ui/react";
+import { Button, Flex, Image, Text } from "@chakra-ui/react";
 import { useState } from "react";
+import TeacherIcon from "./../../assets/icons/teacher.svg";
+import TeamIcon from "./../../assets/icons/team.svg";
 
-const bgColors = ["#6E4AC2", "#00A6CE", "#ff3333", "#ffff00"];
+const bgColors = [
+  "#6E4AC2",
+  "#00A6CE",
+  "#ff3333",
+  "#fdf498",
+  "#fe8a71",
+  "#7bc043",
+  "#4b3832",
+  "#3c2f2f",
+  "#8b9dc3",
+];
 
 function Landing() {
   const [selectedColor, setSelectedColor] = useState(bgColors[0]);
@@ -14,6 +26,7 @@ function Landing() {
         justify={"space-around"}
         align={"center"}
         h={{ base: "0px", md: "0px", lg: "110px" }}
+        position={"fixed"}
       >
         <Flex color={"white"} fontWeight={"bold"} fontSize={"23px"}>
           Mathesis
@@ -37,86 +50,101 @@ function Landing() {
         w={"100%"}
         bg={"#6E4AFF"}
         align={"center"}
-        h={{ base: "0px", xl: "490px", "2xl": "590px" }}
-        direction={"column"}
+        justify={"center"}
+        h={{ base: "0px", xl: "490px", "2xl": "820px" }}
       >
-        <Text
-          color={"white"}
-          fontWeight={"bold"}
-          fontSize={{ base: "0px", md: "48px" }}
-          textAlign={"center"}
-          paddingY={{ base: "0px", md: "48px" }}
-        >
-          Somos melhores juntos!
-        </Text>
+        <Flex w={"60%"} align={"center"} justify={"space-around"}>
+          <Image
+            marginTop={"120px"}
+            src={TeacherIcon}
+            alt={"Professor com alunos"}
+          />
+          <Flex direction={"column"} align={"center"}>
+            <Text
+              color={"white"}
+              fontWeight={"bold"}
+              fontSize={{ base: "0px", md: "48px" }}
+              textAlign={"center"}
+              paddingY={{ base: "0px", md: "48px" }}
+            >
+              Somos melhores juntos!
+            </Text>
 
-        <Text
-          color={"white"}
-          width={{ base: "0px", md: "560px" }}
-          textAlign={"center"}
-          fontSize={{ base: "0px", md: "22px" }}
-        >
-          Mathesis te ajuda a encontrar professores para marcar aulas
-          particulares, de maneira eficiente, fácil e rápida. Diga adeus a suas
-          dúvidas e busque agora por um profissional para te ajudar!
-        </Text>
+            <Text
+              color={"white"}
+              width={{ base: "0px", md: "560px" }}
+              textAlign={"center"}
+              fontSize={{ base: "0px", md: "22px" }}
+            >
+              Mathesis te ajuda a encontrar professores para marcar aulas
+              particulares, de maneira eficiente, fácil e rápida. Diga adeus a
+              suas dúvidas e busque agora por um profissional para te ajudar!
+            </Text>
 
-        <Button
-          marginTop={{ base: "0px", md: "30px" }}
-          borderRadius={"20px"}
-          paddingX={"30px"}
-          paddingY={"25px"}
-          _hover={{
-            color: "white",
-            bg: "black",
-          }}
-        >
-          Criar conta
-        </Button>
+            <Button
+              width={"200px"}
+              marginTop={{ base: "0px", md: "30px" }}
+              borderRadius={"20px"}
+              paddingX={"30px"}
+              paddingY={"25px"}
+              _hover={{
+                color: "white",
+                bg: "black",
+              }}
+            >
+              Criar conta
+            </Button>
+          </Flex>
+        </Flex>
       </Flex>
       <Flex
         w={"100%"}
         bg={"white"}
         align={"center"}
-        h={{ base: "0px", xl: "490px", "2xl": "590px" }}
-        direction={"column"}
+        justify={"center"}
+        h={{ base: "0px", xl: "490px", "2xl": "720px" }}
       >
-        <Text
-          fontWeight={"bold"}
-          fontSize={{ base: "0px", md: "48px" }}
-          textAlign={"center"}
-          paddingTop={{ base: "0px", md: "80px" }}
-          paddingBottom={{ base: "0px", md: "40px" }}
-        >
-          Queremos te ajudar
-        </Text>
-        <Text
-          width={{ base: "0px", md: "560px" }}
-          textAlign={"center"}
-          fontSize={{ base: "0px", md: "22px" }}
-        >
-          Nossa missão é te ajudar da melhor forma possível. Navegue pela
-          plataforma e encontre os melhores profissionais
-        </Text>
-        <Button
-          background={"#6E4AFF"}
-          marginTop={{ base: "0px", md: "50px" }}
-          borderRadius={"20px"}
-          paddingX={"60px"}
-          paddingY={"25px"}
-          color={"white"}
-          _hover={{
-            background: "#6E4AFF",
-          }}
-        >
-          Sobre nós
-        </Button>
+        <Flex w={"65%"} justify={"space-around"}>
+          <Flex direction={"column"} align={"center"}>
+            <Text
+              fontWeight={"bold"}
+              fontSize={{ base: "0px", md: "48px" }}
+              textAlign={"center"}
+              paddingTop={{ base: "0px", md: "80px" }}
+              paddingBottom={{ base: "0px", md: "40px" }}
+            >
+              Queremos te ajudar
+            </Text>
+            <Text
+              width={{ base: "0px", md: "560px" }}
+              textAlign={"center"}
+              fontSize={{ base: "0px", md: "22px" }}
+            >
+              Nossa missão é te ajudar da melhor forma possível. Navegue pela
+              plataforma e encontre os melhores profissionais
+            </Text>
+            <Button
+              background={"#6E4AFF"}
+              marginTop={{ base: "0px", md: "50px" }}
+              borderRadius={"20px"}
+              paddingX={"60px"}
+              paddingY={"25px"}
+              color={"white"}
+              _hover={{
+                background: "#6E4AFF",
+              }}
+            >
+              Sobre nós
+            </Button>
+          </Flex>
+          <Image src={TeamIcon} alt={"Equipe"} />
+        </Flex>
       </Flex>
       <Flex
         w={"100%"}
         align={"center"}
         justify={"space-around"}
-        h={{ base: "0px", xl: "200px", "2xl": "590px" }}
+        h={{ base: "0px", xl: "200px", "2xl": "250px" }}
         bg={"#1F173F"}
       >
         <Button
@@ -139,13 +167,13 @@ function Landing() {
           Mathesis
         </Button>
         <Flex width={"350px"} justify={"space-around"}>
-          <Text color={"white"} fontSize={"18px"}>
+          <Text cursor={"pointer"} color={"white"} fontSize={"18px"}>
             Criar conta
           </Text>
-          <Text color={"white"} fontSize={"18px"}>
+          <Text cursor={"pointer"} color={"white"} fontSize={"18px"}>
             Login
           </Text>
-          <Text color={"white"} fontSize={"18px"}>
+          <Text cursor={"pointer"} color={"white"} fontSize={"18px"}>
             Sobre nós
           </Text>
         </Flex>
