2eef1054a4c0612f2b412d8b6a25c441f91d62b0
Geovane Gallotti Cazal <geovanegallotticazal@hotmail.com>
2023-10-27 19:13:55 -0300

    fix: login

--- src/shared/domain/entities/Student.ts
+++ src/shared/domain/entities/Student.ts
@@ -6,7 +6,6 @@ export default class Student {
   constructor(
     public id: number,
     public userId: number,
-    public user: User,
     public favoriteTeachers: Array<FavoriteTeachers>,
     public createdAt: string,
     public updatedAt: string,
@@ -14,7 +13,6 @@ export default class Student {
   ) {}
 
   static make(data: any): Student {
-    const user = User.make(data.user ?? {});
     const favoriteTeachers = isArray(data.favoriteTeachers)
       ? data.favoriteTeachers.map(FavoriteTeachers.make)
       : ([] as Array<FavoriteTeachers>);
@@ -23,7 +21,6 @@ export default class Student {
       data.id ?? 0,
       data.userId ?? '',
 
-      user,
       favoriteTeachers,
 
       data.createdAt ?? '',
--- src/shared/domain/entities/Teacher.ts
+++ src/shared/domain/entities/Teacher.ts
@@ -1,57 +1,50 @@
-import { isArray } from "@/shared/utils/Array";
-import TeachingSchedules from "./TeachingSchedules";
-import User from "./User";
+import { isArray } from '@/shared/utils/Array';
+import TeachingSchedules from './TeachingSchedules';
+import User from './User';
 
 export default class Teacher {
-    constructor(
-      public id: number,
-      public phone: string,
-      public biography: string,
+  constructor(
+    public id: number,
+    public phone: string,
+    public biography: string,
 
-      public schedules: Array<TeachingSchedules>,
-      public userId: number, 
-      public user: User,
+    public schedules: Array<TeachingSchedules>,
+    public userId: number,
 
-      public city: string, 
-      public state: string, 
-      public badReviews: number, 
-      public mediumReviews: number, 
-      public goodReviews: number, 
-     
-      public createdAt: string,
-      public updatedAt: string,
-      public deletedAt?: string,
-  
-    ) {}
-  
-    static make(data: any): Teacher {
-      const schedules = isArray(data.schedules)
-        ? data.schedules.map(TeachingSchedules.make)
-        : ([] as Array<TeachingSchedules>);
+    public city: string,
+    public state: string,
+    public badReviews: number,
+    public mediumReviews: number,
+    public goodReviews: number,
 
-      const user = User.make(data.user);
-  
-      return new Teacher(
-        data.id ?? 0,
-        data.phone ?? '',
-        data.biography ?? '',
+    public createdAt: string,
+    public updatedAt: string,
+    public deletedAt?: string
+  ) {}
 
-        schedules,
+  static make(data: any): Teacher {
+    const schedules = isArray(data.schedules)
+      ? data.schedules.map(TeachingSchedules.make)
+      : ([] as Array<TeachingSchedules>);
 
-        data.userId ?? 0,
+    return new Teacher(
+      data.id ?? 0,
+      data.phone ?? '',
+      data.biography ?? '',
 
-        user,
+      schedules,
 
-        data.city ?? '',
-        data.state ?? '',
-        data.badReviews ?? 0,
-        data.mediumReviews ?? 0,
-        data.goodReviews ?? 0,
-  
-        data.createdAt ?? '',
-        data.updatedAt ?? '',
-        data.deletedAt,
-      );
-    }
+      data.userId ?? 0,
+
+      data.city ?? '',
+      data.state ?? '',
+      data.badReviews ?? 0,
+      data.mediumReviews ?? 0,
+      data.goodReviews ?? 0,
+
+      data.createdAt ?? '',
+      data.updatedAt ?? '',
+      data.deletedAt
+    );
   }
-  
+}
