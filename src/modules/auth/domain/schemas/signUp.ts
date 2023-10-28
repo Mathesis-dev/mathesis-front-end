f44744bbd72201b36cfa40342923e2084019eb59
Geovane Gallotti Cazal <geovanegallotticazal@hotmail.com>
2023-10-27 23:11:24 -0300

    feat: create signup dto and schema

--- src/modules/auth/domain/schemas/signUp.ts
+++ src/modules/auth/domain/schemas/signUp.ts
@@ -1,30 +1,103 @@
 import { z } from 'zod';
+
+import EUserCategory from '@/shared/domain/enums/EUserCategory';
+import EUserGender from '@/shared/domain/enums/EUserGender';
+
+import { validatePassword } from '@/shared/utils/Password';
 import { formatPhone, validatePhone } from '@/shared/utils/Phone';
 
-export const signUpSchema = z.object({
-  corporateName: z.string().nonempty('Campo obrigatório!'),
-  name: z.string().nonempty('Campo obrigatório!'),
-  stateRegistration: z.string().optional(),
-  phone: z
-    .string()
-    .nonempty('Campo obrigatório')
-    .transform(formatPhone)
-    .refine(validatePhone, 'Telefone inválido!'),
-  email: z
-    .string()
-    .nonempty('Campo obrigatório!')
-    .email('E-mail inválido!')
-    .max(100, 'E-mail muito longo!')
-    .toLowerCase()
-    .trim(),
-  postcode: z.string().nonempty('Campo obrigatório!').trim(),
-  state: z.string().nonempty('Campo obrigatório!'),
-  city: z.string().nonempty('Campo obrigatório!'),
-  cityIbgeCode: z.string().optional(),
-  neighborhood: z.string().nonempty('Campo obrigatório!'),
-  number: z.string().nonempty('Campo obrigatório!'),
-  street: z.string().nonempty('Campo obrigatório!'),
-  reference: z.string(),
-});
+export const signUpSchema = z
+  .object({
+    name: z.string().nonempty('Campo obrigatório!'),
+    email: z
+      .string()
+      .nonempty('Campo obrigatório!')
+      .email('E-mail inválido!')
+      .max(100, 'E-mail muito longo!')
+      .toLowerCase()
+      .trim(),
+    password: z
+      .string()
+      .nonempty('Campo obrigatório!')
+      .min(8, 'Senha de no mínimo 8 caracteres.')
+      .trim()
+      .refine(validatePassword, 'Senha inválida!'),
+    category: z.nativeEnum(EUserCategory, {
+      errorMap: ({ code }) => {
+        if (code === 'invalid_enum_value')
+          return { message: 'Valor inválido!' };
+
+        return {
+          message: 'Campo obrigatório!',
+        };
+      },
+    }),
+    gender: z.nativeEnum(EUserGender, {
+      errorMap: ({ code }) => {
+        if (code === 'invalid_enum_value')
+          return { message: 'Valor inválido!' };
+
+        return {
+          message: 'Campo obrigatório!',
+        };
+      },
+    }),
+    phone: z
+      .string()
+      .optional()
+      .transform((val) => (val ? formatPhone(val) : val))
+      .refine((val) => !val || validatePhone(val), 'Telefone inválido!'),
+    biography: z.string().optional(),
+    state: z.string().optional(),
+    city: z.string().optional(),
+  })
+  .refine(
+    ({ category, phone }) => {
+      if (category === EUserCategory.TEACHER && !phone) {
+        return false;
+      }
+      return true;
+    },
+    {
+      message: 'Campo obrigatório!',
+      path: ['phone'],
+    }
+  )
+  .refine(
+    ({ category, biography }) => {
+      if (category === EUserCategory.TEACHER && !biography) {
+        return false;
+      }
+      return true;
+    },
+    {
+      message: 'Campo obrigatório!',
+      path: ['biography'],
+    }
+  )
+  .refine(
+    ({ category, state }) => {
+      if (category === EUserCategory.TEACHER && !state) {
+        return false;
+      }
+      return true;
+    },
+    {
+      message: 'Campo obrigatório!',
+      path: ['state'],
+    }
+  )
+  .refine(
+    ({ category, city }) => {
+      if (category === EUserCategory.TEACHER && !city) {
+        return false;
+      }
+      return true;
+    },
+    {
+      message: 'Campo obrigatório!',
+      path: ['city'],
+    }
+  );
 
 export type signUpData = z.infer<typeof signUpSchema>;
