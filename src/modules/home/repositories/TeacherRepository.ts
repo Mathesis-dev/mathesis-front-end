e7a91c3401fa6f0b007b1874e06e09a6d593713d
Geovane Gallotti Cazal <geovanegallotticazal@hotmail.com>
2023-10-29 22:01:51 -0300

    feat: create teacher repository

---
+++ src/modules/home/repositories/TeacherRepository.ts
@@ -0,0 +1,38 @@
+import Repository from '@/core/Http/Repository';
+
+import { isArray } from '@/shared/utils/Array';
+
+import IPaginationResponse from '@/shared/domain/interfaces/IPaginationResponse';
+import TeacherListDTO from '../domain/dtos/TeacherListDTO';
+import Teacher from '../domain/entities/Teacher';
+
+export default class TeacherRepository extends Repository {
+  constructor() {
+    super('/teachers');
+  }
+
+  public async list(
+    params: TeacherListDTO
+  ): Promise<IPaginationResponse<Teacher>> {
+    const { status, data } = await this.http.get('/', {
+      params: {
+        ...params.filter,
+        ...params.pagination,
+      },
+    });
+
+    if (this.isOK(status)) {
+      const { meta, data: values } = data;
+
+      return {
+        pages: meta?.pageCount ?? 1,
+        total: meta?.total ?? 0,
+        data: isArray(values)
+          ? values.map(Teacher.make)
+          : ([] as Array<Teacher>),
+      };
+    }
+
+    throw new Error('Ops, algo inesperado aconteceu!');
+  }
+}
